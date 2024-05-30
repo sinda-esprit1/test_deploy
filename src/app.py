26670963d1ccb8f1c44d8ad6e5a9459587ebc7b6
Senda Hamdi <senda.hamdi@resoneo.com>
2024-05-30 12:20:17 +0200

    all_files

---
+++ src/app.py
@@ -0,0 +1,437 @@
+import dash
+from bertopic import BERTopic
+import dash_bootstrap_components as dbc
+from dash import dcc, html, Patch, clientside_callback, callback, dash_table
+from dash.dependencies import Input, Output, State
+from functions import extract_sunburst_data, get_content, llm1claude, get_all_candidats, scraper_resume, llm, llm3v2, get_html_content, extract_links, get_dataframe
+import plotly.graph_objects as go
+import json
+from dash_bootstrap_templates import load_figure_template
+import plotly.io as pio
+import html as ht
+import os
+import pandas as pd
+import os
+
+model_path = os.path.join("backend", "JE_model")
+hierarchy_path = os.path.join("hierarchy", "Hierarchy2.json")
+logo_path=os.path.join("assets","logo.png")
+load_figure_template(["minty", "minty_dark"])
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.MINTY, dbc.icons.BOOTSTRAP], suppress_callback_exceptions=True)
+server = app.server
+color_mode_switch = html.Span(
+    [
+        dbc.Label(className="fa fa-moon", html_for="color-mode-switch"),
+        dbc.Switch(id="color-mode-switch", value=False, className="d-inline-block ms-1", persistence=True),
+        dbc.Label(className="fa fa-sun", html_for="color-mode-switch"),
+    ]
+)
+
+liens_existants = []
+result = ""
+
+
+def load_bertopic():
+    bertopic_model = BERTopic.load(model_path)
+    return bertopic_model
+
+model = load_bertopic()
+
+navbar = dbc.Navbar(
+            dbc.Container(
+                [
+                    dbc.Row([
+                        dbc.Col([
+                              dbc.NavbarBrand("Resoneo AI App", className="ms-2" ,style={"color": "black"})
+                        ],
+                        width={"size":"auto"})
+                    ],
+                    align="center",
+                    className="g-0"),
+
+                    dbc.Row([
+                        dbc.Col([
+                            dbc.Nav([
+                                dbc.NavItem(dbc.NavLink("Dashboard", href="/" ,style={ "color": "black"})),
+
+                                dbc.NavItem(dbc.NavLink("Netlinking", href="/analyzer", style={ "color": "black"})),
+                        
+                            ],
+                            navbar=True
+                            )
+                        ],
+                        width={"size":"auto"})
+                    ],
+                    align="center"),
+                    dbc.Col(dbc.NavbarToggler(id="navbar-toggler", n_clicks=0)),
+                    
+                    dbc.Row([
+                        dbc.Col(
+                             dbc.Collapse(
+                                dbc.Nav([
+                                    
+                                    color_mode_switch
+                                ]
+                                ),
+                                id="navbar-collapse",
+                                is_open=False,
+                                navbar=True
+                             )
+                        )
+                    ],
+                    align="center")
+                ],
+            fluid=True
+            ),
+    dark=True,
+   
+)
+
+
+# Load the sunburst data
+labels, parents, ids, values = [], [], [], []
+with open(hierarchy_path, "r", encoding='utf-8') as f:
+    data = json.load(f)
+for item in data:
+    labels, parents, ids, values = extract_sunburst_data(item)
+fig1 = go.Figure(go.Sunburst(
+    labels=labels,
+    parents=parents,
+    ids=ids,
+    values=values,
+    hoverinfo='label',
+    hovertemplate='<span style="font-size: 20px;">%{label}</span><extra></extra>',
+))
+
+fig1.update_layout(
+    margin=dict(t=0, l=0, r=0, b=0),
+    uniformtext=dict(minsize=10)
+)
+
+fig1.update_traces(textinfo="label+percent parent")
+fig1.update_traces(textfont=dict(size=16))
+
+
+dashboard_layout = dbc.Container([
+    dbc.Row([
+        dbc.Col([
+            dbc.Card([
+                dbc.CardHeader("Sunburst Plot", className="text-center"),
+                dbc.CardBody([
+                    dcc.Graph(id="sunburst-plot", figure=fig1, style={"height": "600px", "width": "100%"}),
+                ]),
+            ], className="mb-4 shadow", style={"cursor": "pointer"}),
+        ], width=6),
+        dbc.Col([
+            dbc.Card([
+
+                dbc.CardHeader([
+                    "Cluster Documents",
+                    html.I(className="bi bi-fullscreen float-end", id="open-modal", style={"cursor": "pointer"})
+                ], className="text-center"),
+                dbc.CardBody([
+                    dash_table.DataTable(
+                        id="cluster-documents",
+                        columns=[],
+                        data=[],
+                        page_size=10,
+                        style_cell={'textAlign': 'left'},
+                        style_table={'overflowX': 'auto', 'maxHeight': '300px'},
+                    ),
+                ]),
+            ], className="mb-4 shadow"),
+            dbc.Card([
+                dbc.CardHeader("Cluster Information", className="text-center"),
+                dbc.CardBody([
+                    html.Div([
+                        html.Div(id="breadcrumb", style={"marginBottom": "10px"}),
+                    ]),
+                  
+                ]),
+            ], className="mb-4 shadow"),
+        ], width=6),
+    ]),
+    dbc.Modal([
+        dbc.ModalHeader(id="modal-header"),
+        dbc.ModalBody([
+            dash_table.DataTable(
+                id="modal-table",
+                columns=[],
+                data=[],
+                page_size=60,
+                style_cell={'textAlign': 'left'},
+                style_table={'overflowX': 'auto', 'maxHeight': '400px'},
+            ),
+            html.Div(id="download-notification")
+        ]),
+        dbc.ModalFooter([
+           
+            html.I(className="bi bi-cloud-download", id="export-table", style={"fontSize": "2rem", "cursor": "pointer"}),
+            dbc.Button("Download Table", id="btn-download-table"),
+            dcc.Download(id="download-table"),
+
+
+            dbc.Button("Close", id="close-modal", className="ms-auto", n_clicks=0),
+        ]),
+    ], id="modal", size="xl", is_open=False),
+], fluid=True)
+analyzer_layout = dbc.Container([
+    dbc.Row([
+        dbc.Col([
+            dbc.Card([
+                dbc.CardHeader("Options"),
+                dbc.CardBody([
+                    dcc.Dropdown(
+                        id="option-dropdown",
+                        options=[
+                            {"label": "From a link", "value": "url"},
+                            {"label": "From content", "value": "contenu"}
+                        ],
+                        value="url",
+                        className="mb-3"
+                    ),
+                    html.Div(id="option-content")
+                ])
+            ], className="mt-4 shadow")
+        ], width=4),
+        dbc.Col([
+            dbc.Card([
+                dbc.CardHeader("Result"),
+                dbc.CardBody([
+                    html.Div(id="content-output", style={'color': 'black'}),
+                    html.Div(id="url-output", style={'color': 'black', 'font-family': 'Arial, sans-serif', 'font-size': '14px'})
+                ])
+            ], className="mt-4 shadow")
+        ], width=8),
+    ]),
+], fluid=True)
+
+# Define the main layout
+app.layout = html.Div([
+    dcc.Location(id="url", refresh=False),
+    navbar,
+    html.Div(id="page-content"),
+])
+
+# Callback to update page content based on URL
+@app.callback(Output("page-content", "children"),
+              [Input("url", "pathname")])
+def display_page(pathname):
+    if pathname == "/":
+        return dashboard_layout
+    elif pathname == "/analyzer":
+        return analyzer_layout
+    else:
+        return "404 Page Not Found"
+
+@app.callback(
+    Output("url-output", "children"),
+    Input("analyze-url-button", "n_clicks"),
+    State("url-input", "value")
+)
+def analyze_url(n_clicks, url):
+    if n_clicks is not None and n_clicks > 0 and url:
+        htmlcontent = get_html_content(url)
+        htmlfinal = htmlcontent.values[0]
+        decoded_html = ht.unescape(htmlfinal)
+        
+        content = get_content(url)
+        themes = llm1claude(content)
+        split = themes.split("\n")
+        theme = [elem for elem in split[1:] if elem != ""]
+        sujets = []
+        for the in theme:
+            sujet, sim = model.find_topics(the, top_n=1)
+            sujets.append(sujet)
+        all = []
+        for element in sujets:
+            if element not in all:
+                all.append(element)
+
+        all_candidats = get_all_candidats(all)
+        liens_existants = extract_links(decoded_html)
+        liens_existants.append(url)
+        liens_a_ajouter = [element for element in all_candidats if element not in liens_existants]
+        to_send = {}
+        for i in liens_a_ajouter:
+            resume = scraper_resume(i)
+            to_send[i] = resume
+        texte = llm(content.values[0], to_send)
+        global result
+        result = llm3v2(decoded_html, texte)
+        return [
+            dbc.CardBody([
+                dbc.Card([
+                    dbc.CardHeader("Existing Links"),
+                    dbc.CardBody([
+                        html.Ul([
+                            html.Li([
+                                html.I(className="bi bi-link-45deg"),
+                                html.A(url, href=url, target="_blank", style={'font-size': '18px'})
+                            ]) for url in liens_existants
+                        ], className="list-unstyled"),
+                    ], className="bg-light")
+                ], className="mb-4 shadow"),
+                dbc.Card([
+                    dbc.CardHeader("Links to Add"),
+                    dbc.CardBody([
+                        dcc.Markdown(texte, dangerously_allow_html=True, style={'font-size': '18px'})
+                    ], className="bg-light")
+                ], className="mb-4 shadow"),
+                dbc.Card([
+                    dbc.CardHeader("Recreated Text"),
+                    dbc.CardBody([
+
+                        dcc.Markdown(result, dangerously_allow_html=True, style={'font-size': '18px'}),
+                               dbc.Row([
+            dbc.Col([
+                dbc.Button("Download HTML",id="btn-download-txt"),
+                dcc.Download(id="download-text")
+              
+            ], className="mb-3"),
+        ]),
+                    ], className="bg-light")
+                ], className="mb-4 shadow")
+            ])]
+
+@app.callback(
+    Output("option-content", "children"),
+    Input("option-dropdown", "value")
+)
+def update_option_content(option):
+    if option == "contenu":
+        return [
+            dbc.Textarea(id="content-input", placeholder="Paste your new content", className="mb-2 form-control"),
+            dbc.Button("Analyze", id="analyze-content-button", color="primary", className="mt-2")
+        ]
+    elif option == "url":
+        return [
+            dbc.Input(id="url-input", placeholder="Enter the link here", className="mb-2 form-control"),
+            dbc.Button("Analyze", id="analyze-url-button", color="primary", className="mt-2")
+        ]
+    else:
+        return 0
+
+@callback(
+    Output("graph", "figure"),
+    Input("color-mode-switch", "value"),
+)
+def update_figure_template(switch_on):
+    template = pio.templates["minty"] if switch_on else pio.templates["minty_dark"]
+    patched_figure = Patch()
+    patched_figure["layout"]["template"] = template
+    return patched_figure
+
+clientside_callback(
+    """
+    (switchOn) => {
+       document.documentElement.setAttribute('data-bs-theme', switchOn ? 'light' : 'dark');  
+       return window.dash_clientside.no_update
+    }
+    """,
+    Output("color-mode-switch", "id"),
+    Input("color-mode-switch", "value"),
+)
+
+@app.callback(
+    Output("cluster-documents", "columns"),
+    Output("cluster-documents", "data"),
+    Output("breadcrumb", "children"),
+    Input("sunburst-plot", "clickData"),
+)
+def update_cluster_documents(click_data):
+    global data
+    
+    if click_data:
+        clicked_id = click_data["points"][0]["id"]
+        clicked_label = click_data["points"][0]["label"]
+        
+        def find_node(node, target_id):
+            if node["id"] == target_id:
+                return node
+            for child in node.get("children", []):
+                found_node = find_node(child, target_id)
+                if found_node:
+                    return found_node
+            return None
+        
+        clicked_node = find_node(data[0], clicked_id)
+        cluster_value = ""
+        if clicked_node and "value" in clicked_node:
+            cluster_value = clicked_node["value"]
+            cluster_value = cluster_value.replace("000", "")
+            df = get_dataframe(int(cluster_value))
+            
+            columns = [
+                {"name": "Link", "id": "url", "presentation": "markdown"},
+                {"name": "Description", "id": "resume"},
+            ]
+
+            dayta = df.to_dict("records")
+ 
+ 
+            breadcrumb = html.Span(" > ".join(clicked_id.split("/")), className="breadcrumb-style")
+            
+            return columns, dayta, breadcrumb
+    
+    return [], [], ""
+# Callback for opening the modal
+@app.callback(
+    Output("modal", "is_open"),
+    [Input("open-modal", "n_clicks"), Input("close-modal", "n_clicks")],
+    [State("modal", "is_open")],
+)
+def toggle_modal(n1, n2, is_open):
+    if n1 or n2:
+        return not is_open
+    return is_open
+
+# Callback for updating the modal content
+@app.callback(
+    Output("modal-header", "children"),
+    Output("modal-table", "columns"),
+    Output("modal-table", "data"),
+    Input("cluster-documents", "data"),
+)
+
+def update_modal_content(data):
+    if data:
+        columns = [
+            {"name": "Title", "id": "url", "presentation": "markdown"},
+            {"name": "Description", "id": "resume"},
+        ]
+        return "Cluster Documents (Full Table)", columns, data
+    return "", [], []
+
+@app.callback(
+    Output("download-table", "data"),
+    [Input("btn-download-table", "n_clicks")],
+    [State("modal-table", "data")],
+    prevent_initial_call=True
+)
+def download_file(n_clicks, data):
+    if n_clicks is not None and n_clicks > 0:
+        if data:
+            df = pd.DataFrame(data)
+            csv_string = df.to_csv(index=False, sep=';')
+            return dict(content=csv_string, filename="cluster.csv")
+
+    return None
+@app.callback(
+    Output("download-text", "data"),
+    [Input("btn-download-txt", "n_clicks")],
+    [State("url-output", "children")],
+    prevent_initial_call=True
+)
+def download_html_file(n_clicks, html_content):
+    if n_clicks is not None and n_clicks > 0:
+        if result:
+            return dict(content=result, filename="result.txt")
+
+
+
+    return None
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run_server(debug=True, port=port)
+    
---
+++ src/functions.py
@@ -0,0 +1,237 @@
+import plotly.express as px
+import plotly as pl 
+from sentence_transformers import SentenceTransformer
+import pandas as pd
+from annoy import AnnoyIndex
+from bertopic import BERTopic
+import numpy as np 
+import plotly.express as px
+import plotly.graph_objects as go
+import anthropic
+import pandas as pd
+import requests
+import html
+import csv
+import re
+import pandas as pd
+from bs4 import BeautifulSoup
+import os
+import os
+from dotenv import load_dotenv
+import pandas as pd
+import openai
+import csv
+
+load_dotenv()
+
+model_path = os.path.join("backend", "JE_model")
+data_path=os.path.join("sources", "data.csv")
+data_us_path=os.path.join("sources", "data_us.csv")
+resume_path=os.path.join("sources", "resume.csv")
+
+def load_bertopic():
+    bertopic_model=BERTopic.load(model_path)
+    return bertopic_model
+model=load_bertopic()
+
+
+
+def extract_sunburst_data(node, path=[], labels=None, parents=None, ids=None, values=None, index=0):
+    if labels is None:
+        labels, parents, ids, values = [], [], [], []
+
+    label = node['label']
+    # Use index for uniqueness in the ID
+    id = "/".join(path + [f"{label}_{index}"])  
+    parent_id = "/".join(path) if path else ""
+    
+    labels.append(label)
+    ids.append(id)
+    parents.append(parent_id)
+    values.append(1)  # Or any other value representing this node
+    
+    node["id"] = id  # Add the unique ID to the node
+
+    new_path = path + [f"{label}_{index}"]
+    for idx, child in enumerate(node.get('children', [])):
+        # Pass idx as index for each child
+        extract_sunburst_data(child, new_path, labels, parents, ids, values, idx)
+    
+    return labels, parents, ids, values
+
+alldata = pd.read_csv(data_path)
+data=pd.read_csv(data_us_path)
+def get_html_content(link) :
+    if link in alldata['url'].values:
+    
+        content = alldata.loc[alldata['url'] == link, 'contenu']
+
+    return  content
+def get_content(link) :
+    if link in data['url'].values:
+    
+        content = data.loc[data['url'] == link, 'titre_contenu']
+
+    return  content
+
+def llm1claude(content):
+    api_key = os.getenv("ANTHROPIC_API_KEY")
+    client = anthropic.Client(api_key=api_key)
+    prompt = f"""Here is a textual content for which I would like you to identify relevant themes to improve the internal linking of similar documents:
+    {content}
+    By analyzing this content, can you propose a list of 4 to 6 expressions that describe the overall content.
+
+    You should respond only with a bulleted list that contains only expressions, without any other descriptive or explanatory text.”
+            """
+
+    response = client.messages.create(
+        model="claude-2.1",
+        temperature=0.6,
+        max_tokens=1500,
+        system=".",
+        messages=[
+            {"role": "user", "content": prompt}
+        ]
+    )
+
+    text = response.content[0].text.strip()
+    return text
+def get_documents_by_topic(dataframe, topic_number):
+    filtered_data = dataframe.loc[dataframe['Topic'] == topic_number]
+    return filtered_data['Document']
+def get_dataframe(cluster_value):
+       
+    data = pd.read_csv(resume_path)
+
+    docs = data["resume"].tolist()
+  
+    df = model.get_document_info(docs)    
+    documents= get_documents_by_topic(df,cluster_value)
+    resultats = []
+    for resume in documents:
+                url_correspondante = data.loc[data['resume'].str.strip() == resume.strip(), 'url']
+
+                link = url_correspondante.iloc[0]
+                index=resume.find("Summary complete")
+                resume = resume[:index] if index != -1 else resume
+             
+                resultats.append({"url": link, "resume": resume})
+    df_resultats = pd.DataFrame(resultats)
+    return df_resultats
+
+def get_all_candidats(topics): 
+    
+
+    data = pd.read_csv(resume_path)
+
+    docs = data["resume"].tolist()
+  
+    df = model.get_document_info(docs)
+
+    cluster_data = {}
+    cluster_options = []
+    links=[]
+    for topic in topics:
+            
+            cluster_name = model.get_topic_info(topic[0]).CustomName.to_string(index=False).strip()
+    
+            cluster_option = f"{cluster_name} "
+            cluster_options.append(cluster_option)
+
+            documents = get_documents_by_topic(df, topic[0])
+            resultats = []
+            for resume in documents:
+                url_correspondante = data.loc[data['resume'].str.strip() == resume.strip(), 'url']
+
+                link = url_correspondante.iloc[0]
+                index=resume.find("Résumé terminé")
+                resume = resume[:index] if index != -1 else resume
+                links.append(link)
+                resultats.append({"URL": link, "Résumé": resume})
+
+            cluster_data[cluster_option] =resultats
+
+
+    return links
+def scraper_resume(url):
+    data=pd.read_csv(resume_path)
+    if url in data['url'].values :
+        content=data.loc[data['url']==url,'resume'].iloc[0]
+        return content
+    
+def llm(content,links):
+
+    api_key = os.getenv("ANTHROPIC_API_KEY")
+    client = anthropic.Client(api_key=api_key)
+    prompt= f"""You are an expert in internal linking of websites. Your task is to analyze a specific content and select the most relevant links from a provided list of candidates, in order to create optimal internal links to improve the structure and navigation of the site.
+    Content to analyze:
+    {content}
+    Candidate links:
+    {links}
+    Instructions:
+    1. Carefully analyze the provided content and identify the key themes and concepts discussed.
+    2. Examine each candidate link and evaluate its relevance to the analyzed content. Note: there are links that are very different, ignore them!
+    3. Select up to 6 links that are the most relevant and can be coherently linked to the content.
+    4. Provide a brief explanation for each selected link, justifying why it is relevant and how it can improve the navigation and user experience of the site.
+    5. Present your response in the form of a list, indicating the selected links and your explanations.
+    6. Keep in mind that the goal is to create a coherent and relevant internal linking structure that helps users easily navigate the site and discover quality related content"""
+    response = client.messages.create(
+        model="claude-3-opus-20240229",
+        temperature=1,
+        max_tokens=3000,
+        system=".",
+        messages=[
+            {"role": "user", "content": prompt}
+        ]
+    )
+
+    text = response.content[0].text.strip()
+    return text
+
+def llm3v2(contenu, liens):
+    api_key = os.getenv("ANTHROPIC_API_KEY")
+    client = anthropic.Client(api_key=api_key)
+    prompt = f'''
+    ###TASK###
+    I will provide you with several elements:
+    - a source article with existing urls
+    - a list of links with a summary of the corresponding target article for each link
+    Use the provided elements to add the links to the source article where it is relevant and in a natural way for readers. Use a link anchor that seems relevant to you in relation to the linked target article.
+
+    Proceed in steps:
+    1. the text contains already some links , leave them as they are 
+    2. Analyze the article and the provided links to understand what each article is about
+    3. Determine the best location for each link within the source article (Integrate the links naturally into the text paragraphs)
+    4. Determine the link anchor to use for each link (The anchor should be short, descriptive and encourage the user to click. Do not use generic anchors like "Learn more".)
+    5. Integrate the links using HTML syntax: <a href="url"style="color: red;"><strong>link anchor</strong></a> (Modify existing content if necessary to naturally integrate the link) 
+    6. Output the full content in HTML format with the integrated links    
+    Here is the content of the source article:
+    """
+    {contenu}
+    """
+    Here is the list of links with a summary of the corresponding target article for each link:
+    """
+    {liens}
+    """
+    ###TASK###
+    '''
+    
+
+    response = client.messages.create(
+        model="claude-3-opus-20240229",
+        temperature=0.3,
+        max_tokens=4000,
+        system=".",
+        messages=[
+            {"role": "user", "content": prompt}
+        ]
+    )
+
+    text = response.content[0].text.strip()
+    return text
+
+def extract_links(text):
+    pattern = r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+    links = re.findall(pattern, text)
+    
+    return links    
